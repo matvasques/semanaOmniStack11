5fa7d6a3fc1ff1206794cfd897f100ce3e88a264
matvasques <matktt@gmail.com>
2020-03-26 06:16:04 -0300

    Front-End do App

--- aulas/frontend/src/index.js
+++ aulas/frontend/src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './App'; //importando o App.js
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/**
+ * ReactDOM.render é uma função utilizada para renderizar elementos html na arvore de objetos(dom)
+ * Estamos renderizando o componente App(app.js), dentro da div id=root(index.html)
+ */
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
+/**
+ * Componente = função que retorna html
+ */
